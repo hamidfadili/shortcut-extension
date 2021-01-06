3a61a6e5748c99401909e4e51c80af4b08414c3d
FADILI Hamid <hamid.fadili@usmba.ac.ma>
2021-01-06 20:24:12 +0100

    render shortcuts dynamically

--- pages/popup/script.js
+++ pages/popup/script.js
@@ -3,6 +3,53 @@ const hiddenComponentClass = "hidden";
 var activeComponent = "page";
 var activeLink = "page-link";
 
+function renderShortcuts() {
+  render(getShortcutes(), "global");
+  render(getShortcutes("youtube.com"), "site");
+  render(getShortcutes("youtube.com", "/watch"), "page");
+}
+
+function render(shortcutes, scope) {
+  shortcutesUI = shortcutes.length
+    ? shortcutes.map((shortcut) => renderShortcut(shortcut)).join("")
+    : "<p>No shortcut added</p>";
+  table = document.createElement("table");
+  table.innerHTML = shortcutesUI;
+  renderSpace = document.querySelector("#" + scope + " .shortcuts-space");
+  console.log(shortcutesUI);
+  renderSpace.appendChild(table);
+}
+
+function renderShortcut(shortcut) {
+  return `
+    <tr>
+        <td>
+            <input value="${shortcut.keys}" type="text">
+        </td>
+        <td>
+            <select name="type">
+                <option selected="${
+                  shortcut.actionType == "click"
+                }" value="click">Click</option>
+                <option selected="${
+                  shortcut.actionType == "focus"
+                }" value="focus">Focus</option>
+                <option selected="${
+                  shortcut.actionType == "open-link"
+                }" value="open-link">Open link</option>
+            </select>
+        </td>
+        <td>
+            ${
+              shortcut.actionType == "open-link"
+                ? `<input type="text" value="${shortcut.target}">`
+                : `<span>${shortcut.target}</span>`
+            }
+        </td>
+    </tr>
+  `;
+}
+
 function updateActive() {
   document.getElementById(componenet).classList.remove(hiddenComponentClass);
   document.getElementById(activeComponent).classList.add(hiddenComponentClass);
@@ -34,6 +81,7 @@ function init() {
     .getElementById(activeComponent)
     .classList.remove(hiddenComponentClass);
   document.getElementById(activeLink).classList.add(activeLinkClass);
+  renderShortcuts();
 }
 
 init();
